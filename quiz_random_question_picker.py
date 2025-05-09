f6b593bd3bd38bf9f48379e5ff44a6f0eb17ea9e
allenandreim <allenandrei8209@gmail.com>
2025-05-09 11:44:56 +0800

    import_modules

--- quiz_random_question_picker.py
+++ quiz_random_question_picker.py
@@ -2,3 +2,10 @@
 # pick random question in quiz creator
 # user will answer it
 # check if the answer is correct
+
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+def load_question(filename):
+    question
