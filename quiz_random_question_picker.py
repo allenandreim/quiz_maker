88bd1ad1f4840822884565bc3e43b20ad1e3da9e
allenandreim <allenandrei8209@gmail.com>
2025-05-09 11:59:52 +0800

    load_question from_chosen_file

--- quiz_random_question_picker.py
+++ quiz_random_question_picker.py
@@ -8,4 +8,25 @@ from tkinter import messagebox
 import random
 
 def load_question(filename):
-    question
+    question = []
+    try:
+        with open(filename, 'r') as file:
+            content = file.read().strip()
+            entries = content.split('---\n')
+            for entry in entries: 
+                lines = entry.strip().split('\n')
+                if len(lines) <  6:
+                    continue
+                q_text = lines[0][len("Question: "):]
+                choices = {
+                    'a': lines[1][3:],
+                    'b': lines[2][3:],
+                    'c': lines[3][3],
+                    'd': lines[4][3]
+                } 
+                answer = lines[5][len("Answer) "):].strip().lower()question.append({
+                    'question': q_text,
+                    'choices': choices,
+                    'answer': answer
+                })
+        
